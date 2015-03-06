97c62db57c6e4dcf256111a28493ef9d6aeb1c3b
farhan1101 <farhanshourov@gmail.com>
2015-03-06 12:54:59 +0600

    Initial Commit

---
+++ CircleApplication/CalculateCircleRelatedOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApplication
+{
+    class CalculateCircleRelatedOperation
+    {
+        public double Diameter { set; get; }
+        public double Perimeter { set; get; }
+
+        public double Area { set; get; }
+
+        public double GetDiameter(double radius)
+        {
+            Diameter = radius*2;
+            return Diameter;
+        }
+
+        public double GetPerimeter(double radius)
+        {
+            return Perimeter = 2*Math.PI*radius;
+        }
+
+        public double GetArea(double radius)
+        {
+            return Area = Math.PI*Math.Pow(radius, 2);
+        }
+
+    }
+}
---
+++ CircleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateCircleRelatedOperation circleCalc = new CalculateCircleRelatedOperation();
+
+            Console.WriteLine("Give the radius of the circle: ");
+            
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The Diameter of the circle is : " + circleCalc.GetDiameter(radius) + "\n");
+            Console.WriteLine("The Perimeterr of the circle is : " + circleCalc.GetPerimeter(radius) + "\n");
+            Console.WriteLine("The Area of the circle is : " + circleCalc.GetArea(radius)+ "\n");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
